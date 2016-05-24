2944d0609d783228d8902bfaa8663138baaa86f6
joreleonardo <jor_el.suarez@yahoo.com>
2016-05-24 16:26:18 +0800

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n  Jorel Leonardo F. Suarez\n\tBS IME-IT\n\t114XXXXX\n     August 16,19XX\n  jorel_pogi@dlsu.edu.ph\n       0917XXXXXXX\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+system("PAUSE");
+return 0;
+}
