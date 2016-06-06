68d2add386e4400235d38f717ebac58ad20a8ee8
joreleonardo <jor_el.suarez@yahoo.com>
2016-06-06 21:37:11 +0800

    Create f(x).c

---
+++ f(x).c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main ()
+{
+	float x,y;
+	printf(" Enter x value: ");
+	scanf("%f", &x);
+	y= x/70;
+	printf("\n f(x) = %.2f\n\n", y);
+	system("PAUSE");
+	return 0;
+}
